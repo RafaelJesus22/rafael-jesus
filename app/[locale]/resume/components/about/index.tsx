35c3d3e595200735f408fb09d57e0c0b1796b7e4
Rafael Jesus <im.13.45.rafael@gmail.com>
2025-08-05 07:32:46 -0300

    feat: add internaciolization to summary section

--- app/[locale]/resume/components/about/index.tsx
+++ app/[locale]/resume/components/about/index.tsx
@@ -4,30 +4,24 @@ import { Strong } from "../ui/strong";
 
 export function AboutSection() {
   const t = useTranslations("Resume");
+  const summary = useTranslations("Resume.aboutSection.summary");
+
+  function getSummary(): string[] {
+    return ["0", "1", "2", "3"].map(summary.raw);
+  }
+
+  function getKey(paragraph: string): string {
+    return paragraph.replace(/\s+/g, "").slice(0, 10);
+  }
 
   return (
     <Section id="about">
       <SectionTitle>{t("aboutSection.sectionTitle")}</SectionTitle>
 
       <div className="px-2 flex flex-col gap-2">
-        <p>
-          I am a software developer with <Strong>over 5 years</Strong> of
-          professional experience, specializing in mobile and web development.
-          Focused on creating beautiful, user-friendly and high performant
-          applications.
-        </p>
-        <p>
-          Throughout my career, I've worked on large-scale, public-facing
-          projects, honing my mobile and frontend development skills. I'm driven
-          by creating solutions that optimize users' lives, always focusing on
-          quality, organization, and technical excellence.
-        </p>
-        <p>
-          I currently work as a Senior Frontend Developer at Jazz Tech, where I
-          contribute to the development of high-performance interfaces and user
-          experiences using technologies such as React Native, Next.js, and
-          Angular, as well as agile practices based on Scrum.
-        </p>
+        {getSummary().map((paragraph) => (
+          <p key={getKey(paragraph)}>{paragraph}</p>
+        ))}
       </div>
     </Section>
   );
